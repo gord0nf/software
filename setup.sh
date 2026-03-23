b7e5e9850a4d9f1a1f48d20085657d1ab43d2b60
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-03-23 12:15:13 -0400

    reorganize setup scripts (single script instead of dir)

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# A `thing` is a software tool or some other, well, thing. Each thing should have a directory in
-# `setup/`, and optionally in `config/`. `setup/${THING}/setup.sh` is the entry point that will be
-# called to setup that thing. `config/${THING}` contains any config stuff for that thing that will
-# be linked by the thing's setup script.
+# A `thing` is a software tool or some other, well, thing. Each thing should have a setup script
+# at `setup/${THING}.sh`, and any config stuff in `config/${THING}/` (optional). The setup script
+# sets the thing up and links anything in `config/${THING}`.
 
 SOFTWARE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-UTILS_SCRIPT="$SOFTWARE_ROOT/setup/utils.sh"
+UTILS_SCRIPT="$SOFTWARE_ROOT/utils.sh"
 
 if (($# != 1)); then
   echo 'usage: setup.sh <thing>'
@@ -14,7 +13,7 @@ if (($# != 1)); then
 fi
 
 thing=$1
-thing_setup_script="$SOFTWARE_ROOT/setup/$thing/setup.sh"
+thing_setup_script="$SOFTWARE_ROOT/setup/$thing.sh"
 thing_config="$SOFTWARE_ROOT/config/$thing"
 
 if ! [[ -f "$thing_setup_script" ]]; then

41086f56eb266b18ee08a89c36ec349ad6468467
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-03-24 12:54:02 -0400

    bash: oh-my-posh command prompt

--- config/bash/profile.sh
+++ config/bash/profile.sh
@@ -9,13 +9,6 @@ alias ls="ls --color=auto"
 alias ll="ls -alh"
 
 ### Cool command prompt ###
-# credit: https://code.mendhak.com/simple-bash-prompt-for-developers-ps1-git/
-
-function parse_git_dirty {
-  [[ $(git status --porcelain 2>/dev/null) ]] && echo "*"
-}
-function parse_git_branch {
-  git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ (\1$(parse_git_dirty))/"
-}
-
-export PS1="\n\t \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+if command -v oh-my-posh &>/dev/null; then
+  eval "$(oh-my-posh init bash --config 'half-life')"
+fi

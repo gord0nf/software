feabafc509583d1506be56fc0712698a04137fc0
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-03-25 07:02:17 -0400

    allow multiple things or --all in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,24 +8,40 @@ SOFTWARE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 UTILS_SCRIPT="$SOFTWARE_ROOT/utils.sh"
 REGISTER_SCRIPT="$SOFTWARE_ROOT/register.sh"
 
-if (($# != 1)); then
-  echo 'usage: setup.sh <thing>'
+if (($# < 1)); then
+  echo 'usage: setup.sh [--all] ...things'
   exit 1
 fi
 
-thing=$1
-thing_setup_script="$SOFTWARE_ROOT/setup/$thing.sh"
-thing_config="$SOFTWARE_ROOT/config/$thing"
-thing_install="$SOFTWARE_ROOT/installed/$thing"
+things=($@)
 
-if ! [[ -f "$thing_setup_script" ]]; then
-  echo "no setup script for '$thing' (would be at '$thing_setup_script')"
-  exit 1
+if [[ "${things[0]}" == '--all' ]]; then
+  things=()
+  for f in $SOFTWARE_ROOT/setup/*.sh; do
+    things+=("$(basename -s '.sh' "$f")")
+  done
 fi
 
 if ! [[ -d "$SOFTWARE_ROOT/installed" ]]; then
   mkdir "$SOFTWARE_ROOT/installed"
 fi
 
-echo "[software] Running setup for '$thing'"
-eval "$thing_setup_script" "$thing_config" "$thing_install" "$UTILS_SCRIPT" "$REGISTER_SCRIPT"
+for thing in "${things[@]}"; do
+  thing_setup_script="$SOFTWARE_ROOT/setup/$thing.sh"
+  thing_config="$SOFTWARE_ROOT/config/$thing"
+  thing_install="$SOFTWARE_ROOT/installed/$thing"
+
+  if ! [[ -f "$thing_setup_script" ]]; then
+    echo "[software] no setup script for '$thing' (would be at '$thing_setup_script')"
+    echo "[software] skipping '$thing'"
+    continue
+  fi
+
+  echo "[software] running setup for '$thing'"
+  eval "$thing_setup_script" "$thing_config" "$thing_install" "$UTILS_SCRIPT" "$REGISTER_SCRIPT"
+  if (($? == 0)); then
+    printf "[software] \e[32m'$thing' setup success\e[0m\n\n"
+  else
+    printf "[software] \e[31m'$thing' setup failed\e[0m\n\n"
+  fi
+done

0576a0de92038c1b59fac64e3fc34c396f860cb1
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-04-12 14:23:40 -0400

    add template

---
+++ setup/template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This is a template for a generic setup script that can be run by /setup.sh.
+# /setup.sh expects usage like `_.sh <config_dir> <install_dir> [--force]`
+
+config_dir=$1
+install_dir=$2
+force=false
+if [[ "$3" == '--force' ]]; then
+  force=true
+fi
+
+UTILS="$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+if ! source "$UTILS"; then
+  echo "fatal: couldn't source $UTILS"
+  exit 1
+fi
+
+echo '[template] this is an empty template'

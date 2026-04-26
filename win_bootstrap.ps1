df861ecebb80fc1a6c9320f27c3f6ce027405435
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-04-26 00:12:17 -0400

    combine windows bootstrap settings into one script

--- win_bootstrap.ps1
+++ win_bootstrap.ps1
@@ -2,6 +2,31 @@ param ( [switch]$Force )
 
 $ProgressPreference = 'SilentlyContinue'
 
+$GitConfigInf = @"
+[Setup]
+Lang=default
+Group=Git
+NoIcons=0
+SetupType=default
+Components=gitlfs,assoc,assoc_sh,windowsterminal,scalar
+Tasks=
+EditorOption=VIM
+CustomEditorPath=
+DefaultBranchOption=main
+PathOption=CmdTools
+SSHOption=OpenSSH
+TortoiseOption=false
+CURLOption=WinSSL
+CRLFOption=LFOnly
+BashTerminalOption=MinTTY
+GitPullBehaviorOption=Rebase
+UseCredentialManager=Enabled
+PerformanceTweaksFSCache=Enabled
+EnableSymlinks=Disabled
+EnablePseudoConsoleSupport=Disabled
+EnableFSMonitor=Disabled
+"@
+
 function Get-VersionTag() {
   $releaseInfo = `
     Invoke-WebRequest -UseBasicParsing "https://api.github.com/repos/git-for-windows/git/releases/latest" `
@@ -15,14 +40,7 @@ function Get-DownloadUrl($Version) {
     if ($Version -like '*.2') {
       $versionNumber += '.2'
     }
-    switch ([System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture) {
-      "X64" { $arch = '64-bit' }
-      "Arm64" { $arch = 'arm64' }
-      default { 
-        throw "OSArchitecture not supported"
-        exit 1
-      }
-    }
+    $arch = (Get-CimInstance Win32_OperatingSystem).OSArchitecture
     return "https://github.com/git-for-windows/git/releases/download/$Version/Git-$versionNumber-$arch.exe"
   } else {
     throw "Couldn't parse version number from tag"
@@ -43,14 +61,21 @@ if (!$Force -and (Get-Command bash -ErrorAction SilentlyContinue)) {
   $tmp = "$tmp.exe"
   Invoke-WebRequest -UseBasicParsing -OutFile "$tmp" "$url"
 
+  $installDir = Join-Path "$PSScriptRoot" "installed\git"
+  New-Item "$installDir" -Type Directory -Force | Out-Null
+  $configFile = New-TemporaryFile
+  Set-Content -Value $GitConfigInf -Path "$configFile"
+
   Write-Host '[bootstrap] installing Git for Windows'
-  $installDir = Join-Path (Split-Path -Parent "$PSScriptRoot") "installed\git"
   Start-Process -Wait -FilePath "$tmp" -ArgumentList @(
     '/SILENT',
     '/NORESTART', 
     '/CURRENTUSER',
     "/DIR=`"$installDir`"",
-    "/LOADINF=`"$PSScriptRoot\git-for-windows.inf`""
+    "/LOADINF=`"$configFile`""
     '/COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"'
   )
+
+  Remove-Item "$configFile" -Force
+  Remove-Item "$tmp" -Force
 }

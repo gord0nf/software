0e7787e676f5c08f3085b5534fe4631dca082ff8
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-04-26 03:28:08 -0400

    software presets

--- setup.sh
+++ setup.sh
@@ -19,6 +19,42 @@ has_element() {
   [[ " ${arr[*]} " =~ [[:space:]]${el}[[:space:]] ]]
 }
 
+add_thing() {
+  if [[ -f "$SOFTWARE_ROOT/src/setup/$1.sh" ]]; then
+    ! has_element things "$1" && things+=("$1")
+  else
+    return 1
+  fi
+  return 0
+}
+
+get_preset_path() {
+  if [[ -f "$1" ]]; then
+    echo "$1"
+  elif [[ -f "$SOFTWARE_ROOT/presets/$1" ]]; then
+    echo "$SOFTWARE_ROOT/presets/$1"
+  else
+    return 1
+  fi
+}
+
+load_preset() {
+  local preset_path=$1
+  local line_no=0
+
+  while IFS=$'\r\n' read -r line; do
+    ((line_no++))
+    if [[ "$line" =~ extends:(.+) ]]; then
+      local extended=$(get_preset_path "${BASH_REMATCH[1]}") &&
+        load_preset "$extended" ||
+        printf "$PREFIX warning: couldn't load line $line_no for $preset_path\n" >&2
+    else
+      add_thing "$line" ||
+        printf "$PREFIX warning: couldn't load line $line_no for $preset_path\n" >&2
+    fi
+  done <"$preset_path"
+}
+
 force=
 things=()
 other_scripts=()
@@ -47,13 +83,15 @@ while (($# > 0)); do
     exit 1
     ;;
   *)
-    if is_script "$1" && ! has_element other_scripts "$1"; then
-      other_scripts+=("$1")
-    elif [[ -f "$SOFTWARE_ROOT/src/setup/$1.sh" ]] && ! has_element things "$1"; then
-      things+=("$1")
+    if is_script "$1"; then
+      ! has_element other_scripts "$1" && other_scripts+=("$1")
+    elif preset_path=$(get_preset_path "$1"); then
+      load_preset "$preset_path"
     else
-      printf "$PREFIX \033[31m'$1' isn't a thing or a script\033[0m\n" >&2
-      printf "$PREFIX \033[33mskipping '$1'\033[0m\n"
+      add_thing "$1" || {
+        printf "$PREFIX \033[31m'$1' isn't a thing, preset, or script\033[0m\n" >&2
+        printf "$PREFIX \033[33mskipping '$1'\033[0m\n"
+      }
     fi
     shift
     ;;
@@ -65,21 +103,17 @@ if ! [[ -d "$SOFTWARE_ROOT/installed" ]]; then
 fi
 
 for thing in "${things[@]}"; do
-  thing_setup_script="$SOFTWARE_ROOT/src/setup/$thing.sh"
+  thing_script="$SOFTWARE_ROOT/src/setup/$thing.sh"
   thing_config="$SOFTWARE_ROOT/src/config/$thing"
   thing_install="$SOFTWARE_ROOT/installed/$thing"
 
-  if bash "$thing_setup_script" "$thing_config" "$thing_install" $force; then
-    printf "$PREFIX \e[32m'$thing' setup success\e[0m\n\n"
-  else
+  bash "$thing_script" "$thing_config" "$thing_install" $force &&
+    printf "$PREFIX \e[32m'$thing' setup success\e[0m\n\n" ||
     printf "$PREFIX \e[31m'$thing' setup failed\e[0m\n\n"
-  fi
 done
 
 for script in "${other_scripts[@]}"; do
-  if bash "$script" '' '' $force; then
-    printf "$PREFIX \e[32m$script success\e[0m\n\n"
-  else
+  bash "$script" '' '' $force &&
+    printf "$PREFIX \e[32m$script success\e[0m\n\n" ||
     printf "$PREFIX \e[31m$thing failed\e[0m\n\n"
-  fi
 done

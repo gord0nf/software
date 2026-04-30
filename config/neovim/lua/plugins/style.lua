290ef48b01388e78f44a6493a602f2eb89804c3b
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-04-29 21:39:45 -0400

    neovim: relative filename and lsp in lualine

--- config/neovim/lua/plugins/style.lua
+++ config/neovim/lua/plugins/style.lua
@@ -24,9 +24,52 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      vim.opt.laststatus = 3 -- global statusline
-      require('lualine').setup()
-    end,
+    opts = {
+      options = {
+        globalstatus = true,
+      },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            file_status = false,
+            path = 1,
+          },
+        },
+        lualine_x = {
+          'import',
+        },
+        lualine_y = {
+          {
+            function()
+              local lsps = vim.lsp.get_clients({ bufnr = vim.fn.bufnr() })
+              local icon = require('nvim-web-devicons').get_icon_by_filetype(
+                vim.api.nvim_get_option_value('filetype', { buf = 0 })
+              )
+              if lsps and #lsps > 0 then
+                local names = {}
+                for _, lsp in ipairs(lsps) do
+                  table.insert(names, lsp.name)
+                end
+                return string.format('%s %s', table.concat(names, ', '), icon)
+              else
+                return icon or ''
+              end
+            end,
+            on_click = function()
+              vim.api.nvim_command('LspInfo')
+            end,
+            color = function()
+              local _, color = require('nvim-web-devicons').get_icon_cterm_color_by_filetype(
+                vim.api.nvim_get_option_value('filetype', { buf = 0 })
+              )
+              return { fg = color }
+            end,
+          },
+          'encoding',
+          'progress',
+        },
+      },
+    },
   },
 }

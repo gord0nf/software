d34cdfc3a470f984eb51138c2bcb8f0d0b48964d
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-26 01:03:33 -0400

    foot: cleaner, user agnostic extended config

--- config/foot.sh
+++ config/foot.sh
@@ -16,17 +16,17 @@ make_directory_link "$CONFIG" "$foot_dir"
 
 # extra configuration -----------------------------------------------------------------------------
 
+extended_foot="$HOME/.foot.ini" # og foot.ini looks for this
+echo >"$extended_foot"
+
 # theme
-rm "$CONFIG/theme.ini" -f &>/dev/null
 if [[ -v ymlconf_config_foot_theme ]]; then
   if [[ -f "$CONFIG/themes/$ymlconf_config_foot_theme.ini" ]]; then
     log "applying theme '$ymlconf_config_foot_theme'"
-    ln -s "$CONFIG/themes/$ymlconf_config_foot_theme.ini" "$CONFIG/theme.ini"
+    echo "include=$CONFIG/themes/$ymlconf_config_foot_theme.ini" >>"$extended_foot"
   else
     warn "invalid theme '$ymlconf_config_foot_theme'"
   fi
-else
-  touch "$CONFIG/theme.ini" # blank file so include directive in foot.ini doesn't fail
 fi
 
 # font
@@ -34,9 +34,13 @@ if [[ -v FONT || -v FONT_SIZE ]]; then
   font=${FONT:-monospace}
   size=${FONT_SIZE:+:size=$FONT_SIZE}
   log "applying font $font$size"
-  echo "font=$font:style=Regular$size
-  font-bold=$font:style=Bold$size
-  font-italic=$font:style=Italic$size" >"$CONFIG/font.ini"
-else
-  touch "$CONFIG/font.ini" # blank file so include directive in foot.ini doesn't fail
+  echo "font=$font:style=Regular$size" >>"$extended_foot"
+  echo "font-bold=$font:style=Bold$size" >>"$extended_foot"
+  echo "font-italic=$font:style=Italic$size" >>"$extended_foot"
+fi
+
+# opacity
+if [[ -v ymlconf_config_foot_opacity ]]; then
+  log "applying opacity '$ymlconf_config_foot_opacity'"
+  echo -e "[colors]\nalpha=$ymlconf_config_foot_opacity" >>"$extended_foot"
 fi

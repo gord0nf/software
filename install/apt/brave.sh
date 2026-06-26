cdf8b0c22e9dc6a2edc79f783947256dea705105
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-25 23:46:51 -0400

    brave: better apt install

--- install/apt/brave.sh
+++ install/apt/brave.sh
@@ -7,17 +7,27 @@ source "$(dirname "${BASH_SOURCE[0]}")/../../utils.sh" || {
   exit 1
 }
 
-[[ $EUID -eq 0 ]] && curl='curl' || {
-  warn 'requires sudo to install apt source'
-  curl='sudo curl'
+apt_package_exists() {
+  dpkg-query -W -f='${Status}' "$1" &>/dev/null
 }
 
-log 'installing apt sources'
-$curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg \
-  https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-$curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources \
-  https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+add_apt_sources() {
+  [[ $EUID -eq 0 ]] && curl='curl' || {
+    warn 'requires sudo to install apt source'
+    curl='sudo curl'
+  }
+
+  $curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg \
+    https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  $curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources \
+    https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+}
+
+apt_package_exists brave-browser || {
+  log 'installing apt sources'
+  add_apt_sources
+  ${APT:-apt} update
+}
 
 log 'installing brave-browser'
-${APT:-apt} update
 ${APT:-apt} install -y brave-browser

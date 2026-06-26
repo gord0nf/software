3b66608ebdae1dba33601a3be2c8d048f6bfb6b1
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-25 22:41:23 -0400

    neovim: better blink autocomplete

--- config/neovim/lua/plugins/code.lua
+++ config/neovim/lua/plugins/code.lua
@@ -1,43 +1,70 @@
+local blink_keymap = {
+  preset = 'none',
+  ['<Tab>'] = { 'select_and_accept', 'fallback' },
+  ['<S-Tab>'] = { 'show', 'hide', 'fallback' },
+  ['<Up>'] = { 'select_prev', 'fallback' },
+  ['<Down>'] = { 'select_next', 'fallback' },
+  ['<C-p>'] = { 'select_prev', 'fallback' },
+  ['<C-n>'] = { 'select_next', 'fallback' },
+  ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+  ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+  ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+  ['<C-u>'] = { 'scroll_signature_up', 'fallback' },
+  ['<C-d>'] = { 'scroll_signature_down', 'fallback' },
+}
+
 local plugins = {
   {
     'saghen/blink.cmp',
     dependencies = { 'rafamadriz/friendly-snippets' },
     version = '*',
-
     ---@module 'blink.cmp'
     opts = {
       fuzzy = { implementation = 'lua' }, -- should be 'prefer_rust' but Windows doesn't like it...
       signature = { enabled = true },
-      keymap = {
-        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-        ['<C-e>'] = { 'hide', 'fallback' },
-        ['<CR>'] = { 'accept', 'fallback' },
-        ['<Tab>'] = { 'select_next', 'fallback' },
-        ['<S-Tab>'] = { 'select_prev', 'fallback' },
-        ['<Up>'] = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
-      },
+      keymap = blink_keymap,
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'normal',
       },
       completion = {
+        keyword = { range = 'full' },
+        accept = {
+          auto_brackets = {
+            enabled = true,
+            kind_resolution = {
+              enabled = true,
+              blocked_filetypes = { 'typescriptreact', 'javascriptreact', 'vue' },
+            },
+          },
+        },
+        list = { selection = { preselect = false } },
+        menu = {
+          auto_show = true,
+          draw = {
+            columns = {
+              { 'label', 'label_description', gap = 1 },
+              { 'kind_icon', 'kind' },
+            },
+            treesitter = { 'lsp' },
+          },
+        },
         documentation = {
           auto_show = true,
-          auto_show_delay_ms = 200,
+          auto_show_delay_ms = 300,
         },
+        ghost_text = { enabled = true },
       },
-      cmdline = { completion = { ghost_text = { enabled = true } } },
-      sources = {
-        default = { 'lsp', 'snippets', 'path', 'buffer' },
+      cmdline = {
+        enabled = true,
+        completion = {
+          menu = { auto_show = true },
+          ghost_text = { enabled = true },
+        },
+        keymap = blink_keymap,
       },
+      sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
     },
-    opts_extend = { 'sources.default' },
   },
   { 'tpope/vim-surround' },
 }

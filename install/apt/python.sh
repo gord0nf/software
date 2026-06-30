e659e2f3a5652526f2a2b4995b78018e460b04df
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-30 16:05:58 -0400

    python: fix apt install

--- install/apt/python.sh
+++ install/apt/python.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-${APT:-apt} install -y python
+MAJOR_VERSION=3
+
+${APT:-apt} install -y python$MAJOR_VERSION python$MAJOR_VERSION-pip python$MAJOR_VERSION-venv ||
+  ${APT:-apt} install -y python pip
